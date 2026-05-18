f31e191869a757626345f7273c08e83cfcbc4c27
anas20023 <anasibnebelal400@gmail.com>
2026-05-18 21:03:08 +0600

    Refactor code structure for improved readability and maintainability

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -15,11 +15,18 @@ export const auth = betterAuth({
     },
     socialProviders: {
         google: {
-            clientId: process.env.GOOGLE_CLIENT_ID ,
+            clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         },
     },
-    plugins:[
+    session: {
+        cookieCache: {
+            enabled: true,
+            strategy: "jwt",
+            maxAge: 5 * 60, 
+        },
+    },
+    plugins: [
         jwt()
     ]
 });
